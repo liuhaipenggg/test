de260d0d61b506925294069e552aff918bad173e
刘海鹏 <liuhaipeng22202@163.com>
2022-12-26 16:40:42 +0800

    持续集成测试

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,12 +1,22 @@
-import { shallowMount } from '@vue/test-utils'
-import HelloWorld from '@/components/HelloWorld.vue'
+// import { shallowMount } from '@vue/test-utils'
+// import HelloWorld from '@/components/HelloWorld.vue'
 
-describe('HelloWorld.vue', () => {
-  it('renders props.msg when passed', () => {
-    const msg = 'new message'
-    const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg }
+// describe('HelloWorld.vue', () => {
+//   it('renders props.msg when passed', () => {
+//     const msg = 'new message'
+//     const wrapper = shallowMount(HelloWorld, {
+//       propsData: { msg }
+//     })
+//     expect(wrapper.text()).toMatch(msg)
+//   })
+// })
+
+import { shallowMount } from '@vue/test-utils'
+import ToDoList from '@/components/ToDoList.vue'
+
+describe('test ToDoList', () => {
+    it('输入框初始值为空字符串', () => {
+        const wrapper = shallowMount(ToDoList)
+        expect(wrapper.vm.toDoText).toBe('')
     })
-    expect(wrapper.text()).toMatch(msg)
-  })
 })
